4468934b2eaf916fcdad0cb42fb604a17a726fe5
Chenyang Li <lcycug@live.com>
2020-02-23 18:10:48 +0800

    channel

--- src/channels/main.go
+++ src/channels/main.go
@@ -3,27 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	links := []string{
-		"https://google.com",
-		"https://facebook.com",
-		"https://amazon.com",
-		"https://twitter.com",
-		"https://golang.org",
+		"https://baidu.com",
+		"https://douban.com",
+		"https://golang.cn",
+		"https://cn.bing.com",
 	}
+	c := make(chan string)
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+
+		go func(l string) {
+			time.Sleep(time.Second)
+			checkLink(l, c)
+		}(l)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
+
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be crashed!")
+		c <- link
 		return
 	}
 
-	fmt.Println(link, "goes well")
+	fmt.Println(link, "goes well.")
+	c <- link
 }
